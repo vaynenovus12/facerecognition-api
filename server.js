2d65e322130e4f2ef4d128972a3377d3129f6af1
Crewzul12 <zulhresident12@gmail.com>
2020-05-24 11:54:08 +0800

    database commit

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '',
-        database: 'facerecognition'
+        connectString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
